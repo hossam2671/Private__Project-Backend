aac87387a195742f555eb442534bae112008d735
hossam2671 <hassonahossam60@gmail.com>
2023-12-28 02:08:24 +0200

    ...

---
+++ Routers/userRoute.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const route = express.Router();
+const user = require("../models/user");
+
+route.post("/addUser",async (req,res)=>{
+    const {role,firstName,lastName,email,phone} = req.body
+    if(!firstName){
+        res.status(400).json({message:"First Name is required"})
+    }
+    else if(!lastName){
+        res.status(400).json({message:"last Name is required"})
+    }
+    else if(!email){
+        res.status(400).json({message:"email is required"})
+    }
+    else if(!phone){
+        res.status(400).json({message:"phone is required"})
+    }
+    else{
+        
+        const userData = await user.create({
+            role:role,
+            firstName:firstName,
+            lastName:lastName,
+            email:email,
+            phone:phone
+        })
+        console.log(userData)
+        res.status(200).json({userData})
+    }
+})
+
+module.exports = route;
+
---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema(
+  {
+    role: {
+      type: String,
+      require: true,
+    },
+    firstName: {
+      type: String,
+      require: true,
+    },
+    lastName: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+    },
+    phone: {
+      type: String,
+      require: true,
+    },
+  },
+  {
+    versionKey: false,
+    strict: false,
+  }
+);
+
+const user = mongoose.model("user", userSchema);
+module.exports = user;
